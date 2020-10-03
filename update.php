825c30a2f5ee819057ac34b77680f1f62e229899
W0153R <41192786+W0153R@users.noreply.github.com>
2020-10-03 09:22:11 +0200

    Oops

--- update.php
+++ update.php
@@ -24,7 +24,7 @@ if($_GET["key"] && $_GET["key"] == $secretKey) {
         $json_sUsed = $json_object->{"seasonalSaltUsed"};
         if (is_numeric($json_dDist) && is_numeric($json_dUsed) && is_numeric($json_sDist) && is_numeric($json_dUsed)) {
           if ($json_sDist > $json_dDist && $json_sUsed > $json_dUsed) {
-            $newAmount =  $json_dUsed;
+            $newAmount =  $json_dDist > 100 && $json_dUsed > 2500 ? $json_dUsed : 0;
           } else {
             $jsonError = "Value error";
           }
